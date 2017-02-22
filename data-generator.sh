af619f1a27f0b83e8bd2374f953d47a5358ec5dc
Marius Bogoevici <mbogoevici@pivotal.io>
2017-02-21 20:12:05 -0500

    Add data generation script

---
+++ data-generator.sh
@@ -0,0 +1,14 @@
+SENSORS=$1
+MIN=$2
+MAX=$3
+RANGE=$(( $MAX - $MIN ))
+while true; do
+for ((i=0; i<=($SENSORS-1); i++)); do
+  VALUE=$(echo "scale=2;$RANDOM/32767 * $RANGE + $MIN + $i" | bc)
+  DATA="{\"id\":$i,\"temperature\":$VALUE, \"city\":\"New York"\"}"
+  echo $DATA
+  curl -i -H "Accept: application/json" -H "Content-Type:application/json"  \
+       -X POST -d $DATA http://localhost:8080
+done
+sleep 0.1
+done
