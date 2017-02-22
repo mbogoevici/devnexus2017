cf799eb67a45337794c1b4d829299e86ccce0126
Marius Bogoevici <mbogoevici@pivotal.io>
2017-02-21 20:41:48 -0500

    Add readme and update data generator

--- data-generator.sh
+++ data-generator.sh
@@ -5,7 +5,7 @@ RANGE=$(( $MAX - $MIN ))
 while true; do
 for ((i=0; i<=($SENSORS-1); i++)); do
   VALUE=$(echo "scale=2;$RANDOM/32767 * $RANGE + $MIN + $i" | bc)
-  DATA="{\"id\":$i,\"temperature\":$VALUE, \"city\":\"New York"\"}"
+  DATA="{\"id\":$i,\"temperature\":$VALUE}"
   echo $DATA
   curl -i -H "Accept: application/json" -H "Content-Type:application/json"  \
        -X POST -d $DATA http://localhost:8080
